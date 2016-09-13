0fcbfe1dadd62b2aa472df094980c2dec1c3528b
lalim9800 <lalim9800@HRH-IC0027328.wrdsb.ca>
2016-09-13 12:31:59 -0400

    2

---
+++ src/A2Q1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lalim9800
+ */
+public class A2Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
