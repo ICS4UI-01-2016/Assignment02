cd128c5d62f59f4fc363796176ade7a1dab4c244
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-09-14 14:13:02 -0400

    made a method to return digital roots of the digits of the number given

--- src/assignment02/Problem2.java
+++ src/assignment02/Problem2.java
@@ -14,13 +14,19 @@ public class Problem2 {
      * @param args the command line arguments
      */
     public int digitalRoot(int n){
+        //seperate the first digit
         int dig1 = n%10;
+        //seperate the second digit
         int dig2 = n/10%10;
+        //seperate the third digit
         int dig3 = n/10/10;
+        //add up the digits
         int dig = dig1+dig2+dig3;
+        //seperate the new number's digits
         int dig4 = dig%10;
         int dig5 = dig/10%10;
         int dig6 = dig/10/10;
+        //add all the new digits
         return dig4+dig5+dig6;
      }
     public static void main(String[] args) {
