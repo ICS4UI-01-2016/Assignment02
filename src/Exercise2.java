eb9dd8b9792140c6880dc04b7cc0e4a9bcd7edbc
tatad6701 <tatad6701@HRH-IC0027318.wrdsb.ca>
2016-09-14 14:23:31 -0400

    Assignment01 (Second Commit)

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -19,6 +19,17 @@ public class Exercise2 {
         return digitalSum(n / 10) + (n % 10);
     }
 
+    public int digitalRoot(int n) {
+        // Chopped off the left side, goes through the algorithem to find out each entered number, then adds to the one beside it
+        int sum = digitalSum(n);
+        // Base case (If it is a single digit)
+        if (sum <= 9) {
+            return sum;
+        }
+        // Run the new sum through digitalSum! Breaking it down and then adding the digits together withing the 2nd return in digitalSum
+        return digitalRoot(sum);
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -26,7 +37,12 @@ public class Exercise2 {
         // TODO code application logic here 
         // Add a new method (Allows us to use antyhing within a class)
         Exercise2 test = new Exercise2();
-        // Print our the user's answer
-        System.out.println(test.digitalSum(126));
+
+        // Print our the user's answer (Question 1)
+        System.out.println(test.digitalSum(0));
+
+        // Print our the user's answer (Question 2)
+        System.out.println(test.digitalRoot(1234567));
+
     }
 }
