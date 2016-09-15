0af654c1d0f06589084f6ca6accc5a2cf82bcfc5
tatad6701 <tatad6701@HRH-IC0027318.wrdsb.ca>
2016-09-15 13:19:30 -0400

    Assignment01 (Fourth Commit)

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -33,8 +33,36 @@ public class Exercise2 {
     public static int triangle(int n) {
         // Create an if statement to focus on the recusion!
         if (n == 0 || n == 1) {
+            // Return the integer, which would return 0 or 1
             return n;
         }
+        // Return the integer plus the integer minus 1 (Getting the block count of the row before the user's input)
+        return n + triangle(n - 1);
+    }
+
+    public static int hailstone(int n) {
+        // Print out all the steps!
+        System.out.println(n);
+        // Base case if the integer is equal to 1, it will print out 1
+        if (n == 1) {
+            // Call the method again, 1 since n is equal to 1 in the if statement
+            return n;
+        }
+        // If the number is even, divide the integer by two (getting the rightmost digit alone) and checking whether is has a decimal (if n < 0)!
+        if (n % 2 == 0) {
+            // Call the method again, the integer that is divided by two
+            return hailstone(n / 2);
+            // If the is user's input is odd 
+        } else {
+            // Call the method again, the integer that is multiplied by three and then one is added to the product
+            return hailstone(3 * n + 1);
+        }
+    }
+
+    public static binaryConvert(int n) {
+        // Create two bases
+        // Return two
+        // Have to flip the binary code
     }
 
     /**
@@ -45,11 +73,21 @@ public class Exercise2 {
         // Add a new method (Allows us to use antyhing within a class)
         Exercise2 test = new Exercise2();
 
-        // Print our the user's answer (Question 1)
+        // Print out the user's answer (Question 1)
+        System.out.println("Question 1:");
         System.out.println(test.digitalSum(0));
 
-        // Print our the user's answer (Question 2)
-        System.out.println(test.digitalRoot(1234567));
+        // Print out the user's answer (Question 2)
+        System.out.println("Question 2:");
+        System.out.println(test.digitalRoot(0));
+
+        // Print out the user's answer
+        System.out.println("Question 3:");
+        System.out.println(triangle(0));
+
+        // Print out the user's answer
+        System.out.println("Question 4:");
+        System.out.println(hailstone(12));
 
     }
 }
