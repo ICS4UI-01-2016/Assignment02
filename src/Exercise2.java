d9210bbe06295933795fcb007fe6223a41db8bd2
richj0985 <richj0985@HRH-IC0027334.wrdsb.ca>
2016-09-15 13:18:33 -0400

    Fifth Commit

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -38,11 +38,35 @@ public class Exercise2 {
         }
     }
     
+    public int hailstone(int n){
+        System.out.println(n);
+        if(n <= 1){
+            return n;
+        } else{
+            if((n % 2) == 0){
+                return hailstone(n/2);
+            } else{
+                return hailstone(3*n+1);
+            }
+        }
+    }
+    
+    public int binaryConvert(int n){
+        if(n <= 1){
+            return n;
+        } else{
+            System.out.print(n % 2);
+            return binaryConvert(n / 2);
+        }
+    }
+    
     public static void main(String[] args) {
         Exercise2 test = new Exercise2();
         System.out.println(test.digitalSum(126));
         System.out.println(test.digitalRoot(2019));
         System.out.println(test.triangle(4));
+        test.hailstone(5);
+        test.binaryConvert(156);
         
     }
 }
