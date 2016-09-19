6a92ecfa7ea8e189483b716ff178736b905641a0
Jon <Jon@Jon-HP>
2016-09-18 21:34:37 -0400

    Home Changes

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -16,49 +16,110 @@ public class Exercise2 {
     public int digitalSum(int n){
         if(n < 10){
             return n;
-        } else {
-            return digitalSum(n / 10) + (n % 10);
         }
+        return digitalSum(n / 10) + (n % 10);
     }
     
     public int digitalRoot(int n){
         int sum = digitalSum(n);
         if(sum < 10){
             return sum;
-        } else{
-            return digitalRoot(sum);
         }
+        return digitalRoot(sum);
     }
     
     public int triangle(int n){
         if(n == 0){
             return n;
-        } else{
-            return triangle(n-1) + n;
         }
+        return triangle(n-1) + n;
     }
     
-    public int hailstone(int n){
+    public void hailstone(int n){
         System.out.println(n);
-        if(n <= 1){
-            return n;
-        } else{
+        if(n > 1){
             if((n % 2) == 0){
-                return hailstone(n/2);
+                hailstone(n/2);
             } else{
-                return hailstone(3*n+1);
+                hailstone(3*n+1);
             }
         }
     }
     
-    public int binaryConvert(int n){
-        if(n / 2 == 0){
-            return n;
-        } else{
-            System.out.print(n % 2);
-            return binaryConvert(n / 2);
+    public String binaryConvert(int n){
+        String value = "";
+        
+        // is number larger than 2, if yes then recurse
+        if(n / 2 != 0){
+            value = binaryConvert(n / 2);
         }
+        // determine the remainder and append the remainder in string form
+        if ( n % 2 == 0 ) {
+            value = value + "0";
+        } else {
+            value = value + "1";
+        }
+        return value;
     }
+
+    
+    public String Convert(int n, int b, int iteration ){
+        String [] digit = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        String value = "";
+ 
+        //System.out.println( n );
+   
+        for (int i=0;i<iteration; i++ ) {
+            System.out.print(">");
+        }
+        System.out.println(" Convert ( " + n +"," + b +")");
+                   
+        // determine if the number is larger than a single digit in the base.
+        // If it is recurse to process the next position/digit
+        if(n >= b){
+            value = Convert(n / b, b, iteration + 1);
+        }
+        
+        for (int i=0;i<iteration; i++ ) {
+            System.out.print(">");
+        }
+        System.out.println(" After-Call to Convert ( " + n +"," + b +")");
+      //  System.out.println( ">" + value );
+        
+        // determine the remainder and append the remainder in string form
+        value = value + digit[ n % b ];
+        
+        return value;
+    }
+    
+//    public void binaryConvert(int n){
+//        if(n / 2 != 0){
+//            binaryConvert(n / 2);
+//        }
+//        System.out.print(n % 2);
+//    }
+    
+//    public int convert(int n, int b) {
+//        
+//    }
+//    
+//    public boolean Palindrome(String s, int length) {
+//        boolean p = true;
+//        if(length == 0 || length == 1 || p == false){
+//            return p;
+//        }
+//        
+//        if(s.charAt(1) == s.charAt(length)){
+//            p = true;
+//            s.substring(2, length - 1);
+//            Palindrome (s, length);
+//        }else{
+//            p = false;
+//        }
+//        
+//        return p;
+//    }
+    
     
     public static void main(String[] args) {
         Exercise2 test = new Exercise2();
@@ -66,8 +127,11 @@ public class Exercise2 {
         System.out.println(test.digitalRoot(2019));
         System.out.println(test.triangle(4));
         test.hailstone(5);
-        test.binaryConvert(156);
-        System.out.println("");
+        System.out.println( test.binaryConvert(1000) );
+//        test.binaryConvert(1000);
+        System.out.println(" 1000 converted:");
+        System.out.println( test.Convert( 123456, 5, 1 ));
+//      System.out.println(test.Palindrome("dad", 3));
         
     }
 }
