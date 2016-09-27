d51459082453e20786bc0c90da21bf2624367303
preej0747 <preej0747@HRH-IC0027339.wrdsb.ca>
2016-09-27 13:12:31 -0400

    assignment 1 completed

---
+++ Methods.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author preej0747
+ */
+public class Methods {
+
+    public double circleArea(double radius){
+        double answer = Math.PI * radius * radius;
+        return answer;
+    }
+    
+    public int fib(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+    
+    public int fact(int n){
+        if(n == 0 || n == 1){
+           return 1; 
+        }
+        return n * fact(n - 1);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Methods test = new Methods();
+        double area = test.circleArea(3);
+        System.out.println("Area: " + area);
+        System.out.println(test.fact(10));
+    }
+}
